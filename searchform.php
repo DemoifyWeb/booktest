8e825887c8ca6cbdbe8da9812719a1323c7074cd
DemoifyWeb <demoify@gmail.com>
2018-05-08 17:16:26 +0545

    m2

--- searchform.php
+++ searchform.php
@@ -32,8 +32,6 @@
  */
 ?>
 
-
-
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ) ?>" >
